2214f3225790c6442818cbf7d00c290c719756ac
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-06 16:51:09 -0300

    fix: route path correction

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/signup', (req, res) => {
+  router.post('signup', (req, res) => {
     res.json({ ok: 'ok' })
   })
 }
