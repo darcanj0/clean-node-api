91edeefbf7ebb2422cd2eb6bf8bb250da50a68d7
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-08 16:39:40 -0300

    test: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,43 @@
+import { HttpRequest, HttpResponse, IController } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log'
+
+interface ISutTypes {
+  controllerStub: IController
+  sut: LogControllerDecorator
+}
+
+const makeSut = (): ISutTypes => {
+  class ControllerStub implements IController {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse = {
+        body: {},
+        statusCode: 200
+      }
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  const controllerStub = new ControllerStub()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut, controllerStub
+  }
+}
+
+describe('Controller Decorator', () => {
+  test('Should call controller handle method', async () => {
+    const { controllerStub } = makeSut()
+
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const sut = new LogControllerDecorator(controllerStub)
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
