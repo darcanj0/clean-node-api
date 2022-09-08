5bada572a0c02e5b91cffa7087a6ce36a40adc80
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-08 11:26:35 -0300

    chore: reconfigure jest files and husky pre-push

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/presentation/controllers/*Controller.ts',
+    '<rootDir>/src/presentation/utils/*.ts',
+    '<rootDir>/src/data/useCases/**/*UseCase.ts'
+  ],
   coverageDirectory: 'coverage',
   roots: ['<rootDir>/src'],
   testEnvironment: 'node',
