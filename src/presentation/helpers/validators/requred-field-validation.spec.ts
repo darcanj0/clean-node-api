4289536891ffd1bcdec1279da7ac203a1e764cf4
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-12 15:32:51 -0300

    test: ensure RequiredFieldValidator returns falsy if validation succeeds

--- src/presentation/helpers/validators/requred-field-validation.spec.ts
+++ src/presentation/helpers/validators/requred-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ objectWithoutRequiredField: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+
+  test('Should return null if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
