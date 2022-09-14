c5f2e74da73b0d2ed2103ecb89d2329b819b0ac3
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-14 15:15:22 -0300

    chore: set different database url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUri: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUri: 'mongodb+srv://daniel:young100@clean-node-api-db.bgtfgvj.mongodb.net/?retryWrites=true&w=majority' || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '1ce992bc-4066-4abb-b759-688a6386e34c'
 }
