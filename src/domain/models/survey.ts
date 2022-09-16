3d290a982afc76e81c6d2d2adf00c5ef91af55b9
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-16 15:23:02 -0300

    feat: ensure SurveyMongoRepository returns a SurveyModel on add success

---
+++ src/domain/models/survey.ts
@@ -0,0 +1,7 @@
+import { SurveyAnswer } from '../usecases/survey/create-survey'
+
+export interface SurveyModel {
+  id: string
+  question: string
+  answers: SurveyAnswer
+}
---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,12 @@
+import { CreateSurveyData, ICreateSurveyRepository } from '../../../../data/usecases/survey/db-create-survey-protocols'
+import { SurveyModel } from '../../../../domain/models/survey'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyMongoRepository implements ICreateSurveyRepository {
+  async add (data: CreateSurveyData): Promise<SurveyModel> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const result = await surveyCollection.insertOne(data)
+    const { insertedId: id } = result
+    return MongoHelper.map(await surveyCollection.findOne({ _id: id }))
+  }
+}
