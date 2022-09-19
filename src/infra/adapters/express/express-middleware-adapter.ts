37da85ae4069394a41e28ab2cdbf8513e5c0c1a6
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-19 15:56:31 -0300

    feat: ensure add survey route returns 403 if no accessToken is provided

---
+++ src/infra/adapters/express/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express'
+import { HttpRequest, IMiddleware } from '../../../presentation/protocols'
+
+export const adaptMiddleware = (middleware: IMiddleware) => {
+  return async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpResponse = await middleware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      return res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express'
+import { adaptMiddleware } from '../../infra/adapters/express/express-middleware-adapter'
 import { adaptRoute } from '../../infra/adapters/express/express-route-adapter'
 import { makeCreateSurveyController } from '../factories/controllers/survey/create-survey/create-survey-controller-factory'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 const surveyRouter = Router()
 
-surveyRouter.post('', async (req, res) => {
-  const route = adaptRoute(makeCreateSurveyController())
-  return await route(req, res)
-})
+surveyRouter.post('', adaptMiddleware(makeAuthMiddleware('admin')), adaptRoute(makeCreateSurveyController()))
 
 export default surveyRouter
