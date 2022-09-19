f1fc7142b137dce705cbe38833898b67c2f091a6
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-19 16:37:55 -0300

    feat: ensure create survey route returns 204 if valid token is provided

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -17,7 +17,8 @@ export class AuthMiddleware implements IMiddleware {
 
   async handle (httpRequest: HttpRequest): Promise <HttpResponse> {
     try {
-      const accessToken = httpRequest.headers?.['x-access-token']
+      const { headers } = httpRequest
+      const accessToken = headers?.['x-access-token']
       if (accessToken) {
         const account = await this.loadAccountByToken.load(accessToken, this.role)
         if (account) {
