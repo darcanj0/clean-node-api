381b6bd2f3aa69fcdb6e11b0034fb44922f09b54
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-21 10:38:22 -0300

    test: ensure DbLoadSurveyById calls repository with correct value

---
+++ src/data/usecases/survey/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,65 @@
+import { DbLoadSurveyById } from './db-load-survey-by-id'
+import { ILoadSurveyByIdRepository, SurveyModel } from './db-load-survey-by-id-protocols'
+import MockDate from 'mockdate'
+
+const makeFakeSurvey = (): SurveyModel => (
+  {
+    id: 'any_id',
+    question: 'any_question',
+    answers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }],
+    date: new Date()
+  }
+)
+
+const makeLoadSurveysRepositoryStub = (): ILoadSurveyByIdRepository => {
+  class LoadSurveyByIdRepositoryStub implements ILoadSurveyByIdRepository {
+    async load (id: string): Promise<SurveyModel> {
+      return new Promise(resolve => resolve(makeFakeSurvey()))
+    }
+  }
+  return new LoadSurveyByIdRepositoryStub()
+}
+
+type SutTypes = {
+  loadSurveyByIdRepositoryStub: ILoadSurveyByIdRepository
+  sut: DbLoadSurveyById
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdRepositoryStub = makeLoadSurveysRepositoryStub()
+  const sut = new DbLoadSurveyById(loadSurveyByIdRepositoryStub)
+  return { sut, loadSurveyByIdRepositoryStub }
+}
+
+describe('LoadSurveyByIdUseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call LoadSurveyByIdRepository', async () => {
+    const { sut, loadSurveyByIdRepositoryStub } = makeSut()
+    const repoSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'load')
+    await sut.loadById('any_id')
+    expect(repoSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  // test('Should throw if LoadSurveyByIdRepository throws', async () => {
+  //   const { loadSurveyByIdRepositoryStub, sut } = makeSut()
+  //   jest.spyOn(loadSurveyByIdRepositoryStub, 'load').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+  //   const promise = sut.loadById('any_id')
+  //   await expect(promise).rejects.toThrow()
+  // })
+
+  // test('Should return a survey on success', async () => {
+  //   const { sut } = makeSut()
+  //   const surveys = await sut.loadById('any_id')
+  //   expect(surveys).toEqual(makeFakeSurvey())
+  // })
+})
