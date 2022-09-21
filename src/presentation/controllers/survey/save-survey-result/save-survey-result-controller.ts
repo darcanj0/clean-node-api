3e67a29cfdd1aba22eb37a353f103f4a66a641d3
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-21 16:37:23 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey/save-survey-result/save-survey-result-controller.ts
@@ -18,15 +18,23 @@ export class SaveSurveyResultController implements IController {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
+      const { answer } = httpRequest.body
+      const { accountId } = httpRequest
       const survey = await this.loadSurveyById.loadById(surveyId)
       if (!survey) {
         return forbidden(new InvalidParamError('surveyId'))
       }
       const possibleAnswers = survey.answers.map((elem) => elem.answer)
-      const isValidAnswer = possibleAnswers.some((answer) => httpRequest.body.answer === answer)
+      const isValidAnswer = possibleAnswers.some((elem) => elem === answer)
       if (!isValidAnswer) {
         return forbidden(new InvalidParamError('answer'))
       }
+      await this.saveSurveyResult.save({
+        accountId,
+        answer,
+        surveyId,
+        date: new Date()
+      })
     } catch (error) {
       return serverError(error)
     }
