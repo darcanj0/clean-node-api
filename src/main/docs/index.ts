333367b49de55c74daaf69ea90e4385dd42b3c69
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-22 14:39:59 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,24 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'CleanNodeApi',
     description: 'Api desenvolvida no curso TDD com Rodrigo Manguinho, para criar e responder enquetes entre programadores',
     version: '1.0.0'
+  },
+  tags: [
+    { name: 'Login' },
+    { name: 'Account' },
+    { name: 'Survey' }
+  ],
+  paths: {
+    '/user/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
---
+++ src/main/docs/paths/login-path.ts
@@ -0,0 +1,30 @@
+export const loginPath = {
+  post: {
+    tags: ['Login'],
+    summary: 'Realiza autenticação de usuário',
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      },
+      400: {
+        description: 'bad request'
+      }
+    }
+  }
+}
