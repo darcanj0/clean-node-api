691e86d9787eb015c51baae91d3fa59981e7d339
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-22 13:41:41 -0300

    feat: setup swagger in /docs and add noCache middleware

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import surveyRouter from '../routes/survey-routes'
 import userRouter from '../routes/user-routes'
 import setupMiddlewares from './middlewares'
+import setupSwagger from './swagger-config'
 
 const app = express()
+setupSwagger(app)
 setupMiddlewares(app)
 app.use('/survey', surveyRouter)
 app.use('/user', userRouter)
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'CleanNodeApi',
+    description: 'Api desenvolvida no curso TDD com Rodrigo Manguinho, para criar e responder enquetes entre programadores',
+    version: '1.0.0'
+  }
+}
