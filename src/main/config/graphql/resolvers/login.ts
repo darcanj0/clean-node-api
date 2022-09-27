0c85a27e941fe7b83ed022d00588606c8c3f6541
darcanj0 <daniel.thomas.aarcanjo@gmail.com>
2022-09-27 10:25:08 -0300

    feat: add signup mutation

--- src/main/config/graphql/resolvers/login.ts
+++ src/main/config/graphql/resolvers/login.ts
@@ -1,8 +1,13 @@
+import { makeSignUpController } from '../../../factories/controllers/account/signup/signup-controller-factory'
 import { adaptResolver } from '../../../../infra/adapters/express/apollo-server-resolver-adapter'
 import { makeLoginController } from '../../../../main/factories/controllers/account/login/login-controller-factory'
 
 export default {
   Query: {
     login: async (parent: any, args: any) => adaptResolver(makeLoginController(), args)
+  },
+
+  Mutation: {
+    signup: async (parent: any, args: any) => adaptResolver(makeSignUpController(), args)
   }
 }
--- src/main/config/graphql/type-defs/login.ts
+++ src/main/config/graphql/type-defs/login.ts
@@ -3,11 +3,13 @@ import { gql } from 'apollo-server-express'
 export default gql`
   extend type Query {
     login(email: String!, password: String!): Account!
+  }
 
+  extend type Mutation {
+    signup(name: String! ,email: String!, password: String!, passwordConfirmation: String!): Account!
   }
 
   type Account {
     accessToken: String!
-    name: String!
   }
 `
